d7b1476857d2bc3ecb3b8647aeaa67da54b03bd5
Danielle Dodd <danielledodd@umail.ucsb.edu>
2016-02-18 18:02:40 -0800

    some changes made to make player.java cleaner

--- src/edu/ucsb/cs56/projects/games/treasure_hunter/Player.java
+++ src/edu/ucsb/cs56/projects/games/treasure_hunter/Player.java
@@ -3,38 +3,40 @@ package edu.ucsb.cs56.projects.games.treasure_hunter;
 import java.util.ArrayList;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
-import java.io.File;
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.Toolkit;
+//import java.io.File;
+//import java.awt.Color;
+//import java.awt.Image;
+//import java.awt.Toolkit;
 
 /**
    A component that draws the Player  by Alex Wood
    
-   @author Alex Wood
-   @version for CS56, W12, UCSB, 2/16/2012
+   @author Alex Wood (UCSB, CS56 W12, 2/16/2012)
+   @author Danielle Dodd and George Lieu
+   @version for CS56, W16, UCSB, 2/18/2016
    
 */
 
 
 public class Player {  
+    // private instance variables
     private boolean movable = true;
     private int xTile;
     private int yTile;
     private int xPos;
     private int yPos;
-    ArrayList<BufferedImage> sprites;
+    private ArrayList<BufferedImage> sprites;
     private int currentSprite = 0;
-
-    
-	
-
-    public Player(int xTile, int yTile, int numSprites, int currentSprite, String name) {
+        
+    //player constructor
+    public Player(int xTile, int yTile, int numSprites,
+		  int currentSprite, String name) {
 	try {
 	    sprites = new ArrayList<BufferedImage>();
 	    for(int i = 0; i < numSprites; i++)
-	    	sprites.add(ImageIO.read(getClass().getResource(GameGui.resourcesDir + name + "/" + name + i + ".png")));
-
+	    	sprites.add(ImageIO.read(getClass().getResource(GameGui.resourcesDir + name
+								+ "/" + name + i + ".png")));
+	    
 	    
 	    this.xTile = xTile;
 	    this.yTile = yTile;
@@ -42,7 +44,7 @@ public class Player {
 	    this.moveTo(xTile*50,yTile*50);
 	} catch (Exception e) {}
     }
-
+    
     public void setSprite(int sprite) {
 	currentSprite = sprite;
     }
@@ -58,10 +60,10 @@ public class Player {
 	this.xPos = xPos;
 	this.yPos = yPos;
     }
-
+    
     public boolean isMovable() {return movable;}
     public void setMovable(boolean movable) {this.movable = movable;}
-
+    
     public int getCurrentSprite() { return currentSprite; }
     public BufferedImage getCurrentImage() { return sprites.get(currentSprite); }
 }
