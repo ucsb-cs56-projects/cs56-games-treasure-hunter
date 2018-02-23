a56abbda07cb23ddd7f2babb193b2362ddc8f0a4
Qilin Liu <565199219@qq.com>
2018-02-22 17:56:09 -0800

    Add multiplayer mode

--- src/edu/ucsb/cs56/projects/games/treasure_hunter/MessageThread.java
+++ src/edu/ucsb/cs56/projects/games/treasure_hunter/MessageThread.java
@@ -3,18 +3,18 @@ package edu.ucsb.cs56.projects.games.treasure_hunter;
 import java.lang.Runnable;
 
 /*
-  Created by Danielle Dodd and George Lieu (CS56, W16, 3/1/2016)
-  Edited by Yusuf Alnawakhtha and Sang Min Oh
-*/
+ Created by Danielle Dodd and George Lieu (CS56, W16, 3/1/2016)
+ Edited by Yusuf Alnawakhtha and Sang Min Oh
+ */
 
 /**
-   The <tt>MessageThread</tt> class takes care of displaying the game's messages.
-   
-   @author George Lieu
-   @author Danielle Dodd
-   @author Yusuf Alnawakhtha and Sang Min Oh
-   @version for UCSB CS56, F17, 11/07/2017
-*/
+ The <tt>MessageThread</tt> class takes care of displaying the game's messages.
+ 
+ @author George Lieu
+ @author Danielle Dodd
+ @author Yusuf Alnawakhtha and Sang Min Oh
+ @version for UCSB CS56, F17, 11/07/2017
+ */
 
 public class MessageThread implements Runnable{
     
@@ -26,28 +26,28 @@ public class MessageThread implements Runnable{
     static int numMessages = 0;
     
     /**
-       Constructs a <tt>MessageThread</tt> object that contains a <tt>GameComponent</tt> object.
-       
-       @param gc <tt>GameComponent</tt> object
-    */
+     Constructs a <tt>MessageThread</tt> object that contains a <tt>GameComponent</tt> object.
+     
+     @param gc <tt>GameComponent</tt> object
+     */
     public MessageThread(GameComponent gc) {
-	this.gc = gc;
+        this.gc = gc;
     }
     
     /**
-       Runs the thread. The message is displayed for 5 seconds before it is erased.
-    */
+     Runs the thread. The message is displayed for 5 seconds before it is erased.
+     */
     public void run() {
-	try{
-	    numMessages++;
-	    Thread.sleep(5000);
-	    numMessages--;
-	    if(numMessages==0 && !gc.winningCondition) {
-		gc.setMessage("");
-	    }
-	}
-	catch(InterruptedException ie) {
-	    System.out.println("Thread was interrupted!");
-	}
+        try{
+            numMessages++;
+            Thread.sleep(5000);
+            numMessages--;
+            if(numMessages == 0 && !gc.winningCondition) {
+                gc.setMessage("");
+            }
+        }
+        catch(InterruptedException ie) {
+            System.out.println("Thread was interrupted!");
+        }
     }
 }
